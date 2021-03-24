a783488c93571505dfaa979d8b858661e9b95c12
Gautam761 <Gautam.anand@student.tafesa.edu.au>
2021-03-24 15:11:12 +1030

    Contact Details
    Added the Contact Details Code.

---
+++ StartFinanceMaster/InstaRichie/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        [Unique]
+        public string PhoneNumber { get; set; }
+
+    }
+}
